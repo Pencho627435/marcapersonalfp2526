bc807e10978238ab55073e18276bcb67ae15639e
Pencho <627435@alu.murciaeduca.es>
2025-11-19 09:50:17 +0100

    ejercios controladores hata getshow

--- resources/views/proyectos/show.blade.php
+++ resources/views/proyectos/show.blade.php
@@ -2,7 +2,49 @@
 
 @section('content')
 
-    Vista detalle proyecto {{$id}}
+    <div class="row m-4">
+
+        <div class="col-sm-4">
+
+            <img src="/images/mp-logo.png" style="height:200px" />
+
+        </div>
+        <div class="col-sm-8">
+
+            {{ $proyecto['docente_id'] }}
+            {{ $proyecto['nombre'] }}
+            <a href="http://github.com/2DAW-CarlosIII/{{ $proyecto['dominio'] }}">
+                http://github.com/2DAW-CarlosIII/{{ $proyecto['dominio'] }}
+            </a>
+
+            @foreach ($proyecto['metadatos'] as $key => $proyector)
+                <div>
+                    {{ $key }} : {{ $proyector }}
+                </div>
+            @endforeach
+
+            @if ($proyecto['metadatos']['calificacion'] < 5)
+                <div>
+                    <strong>“Proyecto suspenso”</strong>
+                    <button type="button" class="btn btn-danger">Aprobar proyecto</button>
+                </div>
+            @elseif ($proyecto['metadatos']['calificacion'] >= 5)
+                <div>
+                    <strong>“Proyecto aprobado”</strong>
+                    <button type="button" class="btn btn-primary">Suspender poyecto</button>
+                </div>
+            @endif
+
+            <div>
+                <a href="" class="button alt">Editar Proyecto</a>
+                <a href="" class="button alt">volver al listado de proyectos</a>
+
+
+            <div>
+
+
+
+        </div>
+    </div>
 
 @stop
-
