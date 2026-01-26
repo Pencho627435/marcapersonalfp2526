223f6d79fa2c75d45bef788c01811fb741f37003
Pencho <627435@alu.murciaeduca.es>
2026-01-26 08:30:36 +0100

    Creacion de relacion 1 a 1 User-Curriculo

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,11 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function curriculo()
+    {
+        return $this->hasOne(Curriculo::class );
+    }
+
+    
 }
